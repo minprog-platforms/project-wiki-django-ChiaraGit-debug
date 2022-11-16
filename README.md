618e44f0bb13550b17c3b21a977b096efb2efc4e
ChiaraProgrammeerd <chiara@LAPTOP-M61H6JBC.localdomain>
2022-11-16 15:59:33 +0100

    design project bijgevoegd

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ Creating an online encyclopedia.
 5. Change the documents
 6. Commit and push
 
-[![index page](C:/Users/Chiara/Documents/Programming/project-wiki-django-ChiaraProgrammeerd/imagesdesignproject/indexpage.jpg?raw=true)]
-[![Create new page](C:/Users/Chiara/Documents/Programming/project-wiki-django-ChiaraProgrammeerd/imagesdesignproject/createnewpage.jpg?raw=true)]
-[![Entry page](C:/Users/Chiara/Documents/Programming/project-wiki-django-ChiaraProgrammeerd/imagesdesignproject/entrypage.jpg?raw=true)]
-[![Error page](C:/Users/Chiara/Documents/Programming/project-wiki-django-ChiaraProgrammeerd/imagesdesignproject/errorpage.jpg?raw=true)]
-[![Search results page](C:/Users/Chiara/Documents/Programming/project-wiki-django-ChiaraProgrammeerd/imagesdesignproject/searchresultspage.jpg?raw=true)]
+[![index page](imagesdesignproject/indexpage.jpg?raw=true)]
+[![Create new page](imagesdesignproject/createnewpage.jpg?raw=true)]
+[![Entry page](imagesdesignproject/entrypage.jpg?raw=true)]
+[![Error page](imagesdesignproject/errorpage.jpg?raw=true)]
+[![Search results page](imagesdesignproject searchresultspage.jpg?raw=true)]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django import forms
+# import entries as md
+# from django import template
+# from django.template.defaultfilters import stringfilter
+import markdownify
 
 from . import util
 
@@ -8,14 +13,49 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+
+# class NewTaskForm(forms.Form):
+#     title = forms.CharField(label="Title")
+#     text = forms.CharField(label="Text")
+
 def createnewpage(request):
-    return render(request, "encyclopedia/createnewpage.html")
+    # if request.method == "POST":
+    #     title = request.POST['givetitle'],
+    #     text = request.POST['givetext'],
+    #     util.save_entry(title, text)
+        return render(request, "encyclopedia/createnewpage.html", {
+            "entries": util.list_entries(),
+            # "form": NewTaskForm()
+        })
+
+        # form = NewTaskForm(request.POST)
+        # if form.is_valid():
+        #     title = form.cleaned_data["title"]
+        #     return render(request, "encyclopedia/index.html", {
+        #         "entries": util.list_entries()
+        #     })
+        # else:
+        #      return render(request, "encyclopedia/createnewpagehtml", {
+        #         "form": form
+        #     })
+
+
+
+
 
 def searchresults(request):
-    return render(request, "encyclopedia/searchresults.html")
+    return render(request, "encyclopedia/searchresults.html", {
+        "entries": util.list_entries()
+    })
 
 def errorpage(request):
-    return render(request, "encyclopedia/errorpage.html")
+    return render(request, "encyclopedia/errorpage.html", {
+        "entries": util.list_entries()
+    })
 
-def entrypage(request):
-    return render(request, "encyclopedia/title.html")
+def entrypage(request, title):
+    return render(request, "encyclopedia/entrypage.html", {
+        "entries": util.list_entries(),
+        "get_entry": util.get_entry(title)
+    })
